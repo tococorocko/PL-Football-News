5f1dee1c4eae411405a9ac6be8e4d6c7c94e7d03
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-24 11:57:40 +0100

    parse Football API in Seed File to get Team Data

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'rest-client'
+gem 'json'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'json'
+
+puts "Clean database"
+puts "..."
+
+Team.destroy_all
+puts "Cleaned"
+
+puts "Get all Teams from API"
+puts "..."
+
+url_teams = 'https://api.football-data.org/v2/competitions/2021/teams'
+club_data_serialized = RestClient.get(url_teams, {'X-Auth-Token' => '0d0c0925a37f431ba7e3bc1feb2a0ef3' })
+club_date = JSON.parse(club_data_serialized)
+
+club_date["teams"].each do |team|
+  Team.create(
+    name: team["name"],
+    crest:
+      if team['crestUrl'] == nil
+        'https://www.shareicon.net/data/2015/12/22/691473_security_512x512.png'
+      else
+        team['crestUrl']
+      end
+  )
+end
+
+#   team["shortName"]
+#   team["website"]
+#   team["clubColors"]
+#   team["venue"]
+
+puts "Done"
