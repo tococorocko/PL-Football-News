d378d5b170832974edc4e6bb34a3869b02aa1adf
Lucca Kaiser <luccakaiser@gmail.com>
2019-01-28 20:06:57 +0100

    style team index

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,7 @@ class Team < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :crest, presence: true, uniqueness: true
+
+  default_scope -> { order(name: :asc) }
+
 end
